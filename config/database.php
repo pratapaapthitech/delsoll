7265eda19bfd7a7fe6daec92f8e584eea107fb9f
Pratap <pjajjara@aapthitech.com>
2018-08-28 19:18:37 +0530

    ssss

--- config/database.php
+++ config/database.php
@@ -1,5 +1,11 @@
 <?php
 
+$url = parse_url(getenv('DATABASE_URL'));
+$host = $url['host'];
+$username = $url['user'];
+$password = $url['pass'];
+$database = substr($url["path"],'1')
+
 return [
 
     /*
@@ -13,7 +19,7 @@ return [
     |
     */
 
-    'default' => env('DB_CONNECTION', 'mysql'),
+    'default' => env('DB_CONNECTION', 'pgsql-stag'),
 
     /*
     |--------------------------------------------------------------------------
@@ -60,6 +66,18 @@ return [
             'database' => env('DB_DATABASE', 'forge'),
             'username' => env('DB_USERNAME', 'forge'),
             'password' => env('DB_PASSWORD', ''),
+            'charset' => 'utf8',
+            'prefix' => '',
+            'schema' => 'public',
+            'sslmode' => 'prefer',
+        ],
+		
+		'pgsql-stag' => [
+            'driver' => 'pgsql',
+            'host' => $host,
+            'database' => $database,
+            'username' => $username,
+            'password' => $password,
             'charset' => 'utf8',
             'prefix' => '',
             'schema' => 'public',
